706ecb669609acf684c1bda61c9bf68661d7de87
Ochoaadev <136945432+Ochoaadev@users.noreply.github.com>
2023-10-20 18:20:15 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Card from './components/Card'
+import Footer from "./components/footer"
 
 function App() {
   return (
     <>
       <NavBar />
       <Card />
+       <Footer/>
     </>
   );
 }
