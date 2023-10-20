55d64ab566b31c43de0786187daf25a98632de4a
JMCA2805 <josemauricio2805@gmail.com>
2023-10-20 00:29:51 -0400

    Agregando el responsive al NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,51 @@
+import { useState } from "react";
+
 function NavBar() {
+  const [width, setWidth] = useState(null);
+
   //Recargar la pagina desde cache
   const reload = () => {
     window.location.reload(false);
   };
+  // Obtener la resolucion
+  const resolution = () => {
+    setWidth(window.innerWidth);
+  };
+  // Actualizar la Resolucion
+  window.addEventListener("resize", () => {
+    resolution();
+  });
 
   return (
     <>
       {/* Creacion del NavBar */}
-      <nav className="h-16 w-full bg-white shadow-lg flex">
+      <nav className="h-16 w-full bg-white shadow-lg flex ssm:h-12">
         {/* Contenedor del logo */}
-        <div className="w-1/6 h-full flex justify-center items-center">
+        <div className="w-1/6 h-full flex justify-center items-center md:w-1/4 ssm:w-1/4">
           <button
             onClick={() => {
               reload();
             }}
           >
-            <img src="src\assets\logo.png" alt="Logo" className="h-12" />
+            <img
+              src="src\assets\logo.png"
+              alt="Logo"
+              className="h-12 ssm:h-8"
+            />
           </button>
         </div>
         {/* Contenedor del Buscar */}
-        <section className="w-4/6 flex justify-center items-center h-full">
+        <section className="w-4/6 flex justify-center items-center h-full md:w-3/5 ssm:w-2/4">
           {/* Buscador */}
           <input
             type="text"
-            className="w-3/6 h-10 text-lg text-gray font-medium px-6 rounded-full border-2 border-azure focus:outline-none focus:border-midnight-blue focus:text-purple-navy"
+            className="w-3/6 h-10 text-lg text-gray font-medium px-6 rounded-full border-2 border-azure focus:outline-none focus:border-midnight-blue focus:text-purple-navy ssm:w-3/5 ssm:h-8"
             placeholder="Buscar"
           />
           {/* Boton de Buscar */}
           <button
             type="submit"
-            class="flex items-center bg-azure justify-center w-10 h-10 mx-2 rounded-full hover:bg-purple-navy focus:bg-midnight-blue border-b-4 border-midnight-blue"
+            className="flex items-center bg-azure justify-center w-10 h-10 mx-2 rounded-full hover:bg-purple-navy focus:bg-midnight-blue border-b-4 border-midnight-blue ssm:w-8 ssm:h-8"
           >
             <img
               src="src\assets\icons\search-white.png"
@@ -39,21 +55,30 @@ function NavBar() {
           </button>
         </section>
         {/* Contenedor de botones */}
-        <div className="flex items-center justify-center w-1/6 ">
+        <div className="flex items-center justify-center w-1/6 md:w-1/4 ssm:w-1/5">
           {/* Contenedor del boton Agregar */}
-          <div className="flex items-center justify-center w-1/2">
+          <div className="flex items-center justify-center w-1/2 ">
             <button
               type="submit"
-              class="flex items-center text-center bg-azure justify-center h-10 px-4 mx-2 rounded-full hover:bg-purple-navy focus:bg-midnight-blue border-b-4 border-midnight-blue"
+              className="flex items-center text-center bg-azure justify-center h-10 px-4 mx-2 rounded-full hover:bg-purple-navy focus:bg-midnight-blue border-b-4 border-midnight-blue ssm:w-8 ssm:h-8"
             >
-              <span className="text-white font-medium">Agregar</span>
+              {/* Segun la resolucion el contenido del boton cambia */}
+              {width <= 575 ? (
+                <img
+                  src="src\assets\icons\plus.png"
+                  alt="+"
+                  className="p-2"
+                />
+              ) : (
+                <span className="text-white font-medium">Agregar</span>
+              )}
             </button>
           </div>
           {/* Contenedor del boton de Modo Oscuro-Claro */}
           <div className="flex items-center justify-end w-1/2">
             <button
               type="submit"
-              class="flex items-center text-center bg-azure justify-center w-10 h-10 mx-6 rounded-full hover:bg-purple-navy focus:bg-midnight-blue border-b-4 border-midnight-blue"
+              className="flex items-center text-center bg-azure justify-center w-10 h-10 mx-6 rounded-full hover:bg-purple-navy focus:bg-midnight-blue border-b-4 border-midnight-blue ssm:w-8 ssm:h-8  ssm:mx-0"
             >
               <img
                 src="src\assets\icons\moon.png"
