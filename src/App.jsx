7a8a4a848cf93b2752df1fb2ae438695afd75e3c
CJPM27 <126300063+CJPM27@users.noreply.github.com>
2023-10-20 05:32:11 -0400

    Organización y codificación del Modal 1era parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import NavBar from './components/NavBar'
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
     <>
-    <NavBar />
+      <NavBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
