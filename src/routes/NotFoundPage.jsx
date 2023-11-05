2abad628b6f342bfd31de085b0faec03378444e7
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-11-04 21:47:57 -0400

    Routes sobre Pagina no encontrada

--- src/routes/NotFoundPage.jsx
+++ src/routes/NotFoundPage.jsx
@@ -1,9 +1,20 @@
+import Nav from "../components/NavBar";
+import Footer from "../components/Footer";
+
 export default function NotFoundPage() {
     return(
-        <div>
-            <span>
-                Error 404
-            </span>
-        </div>
+        <>
+            <Nav />
+                <div>
+                    
+                    <h1 className="text-9xl mt-6 mr-4 font-bold text-right">
+                        404 
+                    </h1>         
+                    <h1 className="text-9xl mt-6 mr-4 font-bold text-right">
+                        Página no encontrada
+                    </h1>  
+                </div>
+            <Footer />
+        </>
     )
 }
