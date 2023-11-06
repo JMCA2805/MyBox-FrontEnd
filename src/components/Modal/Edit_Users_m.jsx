516c0b467f29cf9c449f852b1c1ccec4b4a27fb4
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-11-06 00:12:14 -0400

    Add files via upload

---
+++ src/components/Modal/Edit_Users_m.jsx
@@ -0,0 +1,29 @@
+import { Modal, Button, ModalHeader, ModalBody, ModalFooter } from "./Modal";
+import { useItemsContext } from "../../contexts/UpProvider";
+import { useEffect, useState } from "react";
+
+function Edit_User() {
+   const {
+    openEdit_user,
+    handleOpenEdit_user,
+    } = useItemsContext();
+   return (
+      <>
+         <Modal open={openEdit_user} handleOpen={handleOpenEdit_user}>
+            <form className="flex-col flex px-4 justify-center items-center"
+               onSubmit={async (e) => {
+                  Editar(e);
+               }}
+               id="form_edit_user"
+               encType="multipart/form-data"
+            >
+                <div>
+                    text
+                </div>
+            
+            </form>
+        </Modal>
+      </>
+  );
+}
+export default Edit_User;
